3dd47165ff6031f2d349d0513deedc389fc0e549
Joseph Broome <josephlloydbroome@gmail.com>
2022-07-20 21:54:34 -0700

    updates

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StaticExercise;
+
+var celcius=TermpConverter.FarenheitToCelcius(68);
+
+var farenheit = TermpConverter.CelciusToFaranehit(20);
+
+Console.WriteLine($"the temp in celcius is {celcius}");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"the temp in farenheit is {farenheit}");
---
+++ TermpConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TermpConverter
+    {
+        public static double FarenheitToCelcius(double farenheit)
+        {
+            var result= (farenheit - 32) / 1.8;
+            return result;
+        }
+
+        public static double CelciusToFaranehit(double celsius)
+        {
+            var result= (celsius * 9) / 5 + 32;
+            return result;
+        }
+    
+    
+    }
+}
